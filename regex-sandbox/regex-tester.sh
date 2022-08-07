593747d5885e4515ddb12d81983d8a58ea21c3e5
atrox3d <atrox3d@gmail.com>
2022-08-07 09:33:45 +0200

    fix regex tr

--- regex-sandbox/regex-tester.sh
+++ regex-sandbox/regex-tester.sh
@@ -1,23 +1,36 @@
 #!/usr/bin/env bash
 
 
-if [ ! -t 0 -a $# -ge 1 ]			# pipe input stdin
+if [ ! -t 0 -a $# -ge 1 ]
 then
-	input=-
-	regex="$1"
-elif [ -t 0 -a $# -ge 2 ]			# filename in $1
+	input=-										# pipe input stdin
+	regex="${1}"								# regex in first param
+elif [ -t 0 -a $# -ge 2 ]
 then
-	input="$1"
-	regex="$2"
+	input="${1}"								# input filename in ${1}
+	regex="${2}"								# regex in second param
 else
-	echo "syntax | command | $0 regex"
-	echo "syntax | $0 filename regex"
+	echo "syntax | command | ${0} regex"
+	echo "syntax | ${0} filename regex"
 	exit 255
 fi
 
-echo "input | $input"
-echo "regex | $regex"
+input="$(cat "${input}" | tr $'\n' ' ' )"		# assign input
 
-cat "$input" | tr $'\n' ' ' | egrep -qi "$regex" && echo found || echo not found
+if [ -f "${regex}" ]							# 2nd parameter is filename
+then
+	echo "${regex} file found"
+	regex="$(cat "${regex}" | tr -d $'\n')"		#  assign regex file content
+fi
+
+echo "input | '${input}'"
+echo "regex | '${regex}'"
+
+echo "${input}" | egrep -qi "${regex}" && {
+	echo found
+} || {
+	echo not found
+}
+
 
 
