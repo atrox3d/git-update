158a238808ca9aba635e98b90cdc28222b12583a
atrox3d <atrox3d@gmail.com>
2022-08-07 07:16:02 +0200

    add regex-tester.sh

---
+++ regex-sandbox/regex-tester.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+
+if [ ! -t 0 -a $# -ge 1 ]			# pipe input stdin
+then
+	input=-
+	regex="$1"
+elif [ -t 0 -a $# -ge 2 ]			# filename in $1
+then
+	input="$1"
+	regex="$2"
+else
+	echo "syntax | [ command | ] $0 regex"
+	echo "syntax | $0 filename regex"
+	exit 255
+fi
+
+echo "input | $input"
+echo "regex | $regex"
+
+cat "$input" | tr $'\n' ' ' | egrep -qi "$regex" && echo found || echo not found
+
+
