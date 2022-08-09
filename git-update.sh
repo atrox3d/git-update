b6955a7262be7ec98d84a234484f55a6b7bb768d
atrox3d <atrox3d@gmail.com>
2022-08-09 03:30:22 +0200

    move colors to lib/color.include

--- git-update.sh
+++ git-update.sh
@@ -11,72 +11,25 @@
 #		if the repo needs pull/add/commit/... it displays git output
 #
 ###############################################################################
-#
-#	colorful colors, yay...
-#
-function ansi_escape()
-{
-	# local code="${1}"
-	
-	for code
-	do
-		echo -n "\e[${code}m"
-	done
-}
-
-readonly ANSI_RESET=0
-readonly ANSI_LIGHT=1
-readonly ANSI_DARK=0
-
-readonly ANSI_FG=3
-readonly ANSI_BG=4
-readonly ANSI_REVERSE=7
-
-readonly ANSI_RED=1
-readonly ANSI_GREEN=2
-readonly ANSI_YELLOW=3
-
-readonly COLOR_RESET="$(ansi_escape ${ANSI_RESET})"
-readonly COLOR_LIGHT_RED="$(ansi_escape ${ANSI_LIGHT} ${ANSI_FG}${ANSI_RED})"
-readonly COLOR_LIGHT_GREEN="$(ansi_escape ${ANSI_LIGHT} ${ANSI_FG}${ANSI_GREEN})"
-readonly COLOR_LIGHT_YELLOW="$(ansi_escape ${ANSI_LIGHT} ${ANSI_FG}${ANSI_YELLOW})"
-
-readonly COLOR_REV_RED="$(ansi_escape ${ANSI_REVERSE})${COLOR_LIGHT_RED}"
-readonly COLOR_REV_GREEN="$(ansi_escape ${ANSI_REVERSE})${COLOR_LIGHT_GREEN}"
-readonly COLOR_REV_YELLOW="$(ansi_escape ${ANSI_REVERSE})${COLOR_LIGHT_YELLOW}"
-
-function color()
-{
-	local _intensity="${1^^}"
-	shift
-	local _color="${1^^}"
-	shift
-	local _text="$*"
-	
-	local _color_name="COLOR_${_intensity}_${_color}"
-	
-	[[ -v ${_color_name} ]] || {
-		echo "FATAL | unknow color ${_color_name}"
-		exit 255
-	}
-	
-	echo "${!_color_name}${_text}${COLOR_RESET}"
-	
-}
-
 # HERE="$(dirname ${BASH_SOURCE[0]})"
-
 HERE="$(dirname ${BASH_SOURCE[0]})"
-if which realpath
+if which realpath |& /dev/null
 then
 	HERE="$(realpath ${HERE})"
-elif which readlink
+elif which readlink |& /dev/null
 then
 	HERE="$(readlink -f ${HERE})"
 else
 	echo "FATAL | cannot determine script absolute path"
 	exit 255
 fi
+###############################################################################
+COLOR_INCLUDE="${HERE}/lib/color.include"
+.  "${COLOR_INCLUDE}" || {
+	echo "FATAL | cannot source ${COLOR_INCLUDE}"
+	exit 255
+}
+###############################################################################
 REGEX_DIR="${HERE}/regex-sandbox"
 PULL_ENABLED="false"
 STOP_AT_FIRST="false"
